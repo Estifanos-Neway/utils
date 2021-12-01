81e2a5fe714de71f34da181c74a2e58b4bc6f4dc
StivsGit <88480740+StivsGit@users.noreply.github.com>
2021-12-01 09:53:40 +0300

    Create utils.js
    create the initial util.js file

---
+++ JS/utils.js
@@ -0,0 +1,56 @@
+
+/**
+ * this module contains utilities (functions)
+ * to be used by other programs.
+ * */
+class log{
+    #normalLog;
+    #errorLog;
+    constructor(normalLog = process.stdout, errorLog = process.stderr){
+        setNormalLog(normalLog);
+        setErrorLog(errorLog);
+    }
+    set setNormalLog(stream){
+        this.#normalLog = stream;
+    }
+    get getNormalLog(){
+        return this.#normalLog;
+    }
+    set setErrorLog(stream){
+        this.#errorLog = stream;
+    }
+    get getErrorLog(){
+        return this.#errorLog;
+    }
+}
+ function clog(data){
+     /**
+      * this is a short hand to console.log() vbvc
+      */
+     return console.log(data);
+ }
+
+function wait(duration){
+     /**
+      * this will return a promise to be
+      * fullfiled after the given duration
+      */
+     return new Promise((resolve) =>{
+         setTimeout(resolve,duration)
+     })
+ }
+
+ async function waitThenGet(duration,processor,...args){
+     /**
+      * this will wait for the given duration
+      * then return the return value of the
+      * given function after execute it with
+      * the given aguments.
+      */
+     await wait(duration);
+     return processor(...args);
+ }
+
+ exports.clog = clog;
+ exports.wait = wait;
+ exports.waitThenGet = waitThenGet;
